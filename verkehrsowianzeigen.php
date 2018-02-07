98c09198507c6b5aa32db17f04271e77f3a0dd20
falschparkenffm <unknwon>
2018-02-07 21:37:33 +0100

    Add seperate mail template
    Closes #3

--- verkehrsowianzeigen.php
+++ verkehrsowianzeigen.php
@@ -9,10 +9,44 @@
 <ol>
 <li>Foto der Situation machen, auf dem sowohl das Fahrzeug inkl. Kennzeichen und der Weg/Beschilderung zu sehen ist</li>
 <li>Adresse inkl. Hausnummer merken/notieren (das spart sp&auml;ter Arbeit)</li>
-<li>Anzeige bei Polizeirevier der Wahl oder <a href="mailto:owi.datenerfassung.amt32@stadt-frankfurt.de?Subject=Anzeige%20einer%20Verkehrsordnungswidrigkeit&amp;Body=Tattag%3A%20%0A%0ATatzeit%3A%20%0A%0ATatort%3A%20%0A%0AKfz-Kennzeichen%3A%20%0A%0Agenauer%20Tatvorwurf%3A%20%0A%0Avollst&auml;ndiger%20Name%20und%20Anschrift%20des%20Anzeigenden%3A%20%0A%0AEin%20Beweisfoto%2C%20aus%20dem%20Kennzeichen%20und%20Tatvorwurf%20erkennbar%20hervorgehen%2C%20befindet%20sich%20im%20Anhang.">direkt per E-Mail beim Ordnungsamt stellen</a> <a href="#quellen_2"><sup>2</sup></a> <a href="#quellen_3"><sup>3</sup></a></li>
+<li>Anzeige bei Polizeirevier der Wahl, <a href="mailto:owi.datenerfassung.amt32@stadt-frankfurt.de?Subject=Anzeige%20einer%20Verkehrsordnungswidrigkeit&amp;Body=Tattag%3A%20%0A%0ATatzeit%3A%20%0A%0ATatort%3A%20%0A%0AKfz-Kennzeichen%3A%20%0A%0Agenauer%20Tatvorwurf%3A%20%0A%0Avollst&auml;ndiger%20Name%20und%20Anschrift%20des%20Anzeigenden%3A%20%0A%0AEin%20Beweisfoto%2C%20aus%20dem%20Kennzeichen%20und%20Tatvorwurf%20erkennbar%20hervorgehen%2C%20befindet%20sich%20im%20Anhang.">direkt per mailto-Link beim Ordnungsamt stellen</a> oder die <a href="#mail_vorlage">Mail-Vorlage</a> nutzen. <a href="#quellen_2"><sup>2</sup></a> <a href="#quellen_3"><sup>3</sup></a></li>
 <li>Fertig! Alles weitere &uuml;bernimmt die Stadt. Man bekommt generell keinerlei R&uuml;ckmeldung!</li>
 </ol>
 <p>Da die <a href="https://www.wegeheld.org">Wegeheld-App</a> auch (falls gew&uuml;nscht) nur eine E-Mail generiert sollte auch diese Nutzung ohne Probleme funktionieren. Hierbei muss allerdings die korrekte Empf&auml;ngeradresse gepr&uuml;ft werden.</p>
+
+<h1 id="mail_vorlage">E-Mail-Vorlage</h1>
+<p>
+Anstatt des oben verwendeten Direktlinks zum Mail-Programm kann auch folgender Text in eine Mail kopiert und an <i>owi.datenerfassung.amt32@stadt-frankfurt.de</i> werden:
+<br>
+<i>
+Tattag: 01.01.2018
+<br>
+<br>
+Tatzeit: 18:00 Uhr
+<br>
+<br>
+Tatort: Musterstrasse 123
+<br>
+<br>
+Kfz-Kennzeichen: [Kennzeichen]
+<br>
+<br>
+Kfz-Farbe und -Marke: Blauer [Automarke]
+<br>
+<br>
+genauer Tatvorwurf: Halten/Parken auf einem Radweg/Fu&szlig;weg mit Behinderung von Radfahrern/Fu&szlig;g&auml;ngern, die ausweichen mussten.
+<br>
+<br>
+vollst&auml;ndiger Name und Anschrift des Anzeigenden:<br>
+[Vorname] [Nachname]<br>
+[Strasse] [Hausnummer]<br>
+[PLZ] [Wohnort]<br>
+<br>
+<br>
+Ein Beweisfoto, aus dem Kennzeichen und Tatvorwurf erkennbar
+hervorgehen, befindet sich im Anhang.
+</i>
+</p>
 
 <h1 id="quellennachweise">Quellennachweise</h1>
 <sup id="quellen_1">1</sup> <a href="https://twitter.com/Stadt_FFM/status/930424805014278144">Aussage der Stadt Frankfurt auf Twitter vom 14.11.2017</a>, zuletzt gepr&uuml;ft am 07.02.2018 12 Uhr
