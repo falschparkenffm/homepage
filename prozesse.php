5031d56d385f0739ed05fa1d30776c49f484c256
falschparkenffm <unknwon>
2018-02-08 10:01:55 +0100

    Fix layout and add new information

--- prozesse.php
+++ prozesse.php
@@ -2,11 +2,16 @@
 <title>Interne Abl&auml;ufe bei Polizei und Stadt Frankfurt</title>
 </head>
 <?php include ("menu.html"); ?>
-<p>Diese Seite soll nur einige Hintergrundinformationen bereitstellen - diese sind f&uuml;r die allt&auml;gliche Arbeit nicht erforderlich!</p>
-<h1>Organisation der Abschleppvorg&auml;nge</h1>
-Die Polizei Frankfurt organisiert ihre Abschleppauftr&auml;ge &uuml;ber die <a href="http://www.abschleppdienste-frankfurt.de">Firma Noss</a> <a href="#quellen_1"><sup>1</sup></a>.
+<p>Diese Seite soll nur einige Hintergrundinformationen bereitstellen - diese sind f&uuml;r die allt&auml;gliche Arbeit normalerweise nicht erforderlich.</p>
+<h1>Schutzpolizei: Organisation der Abschleppvorg&auml;nge</h1>
+<p>Die Polizei Frankfurt organisiert ihre Abschleppauftr&auml;ge &uuml;ber die <a href="http://www.abschleppdienste-frankfurt.de">Firma Noss</a> <a href="#quellen_1"><sup>1</sup></a>.</p>
+
+<h1>Stadt Frankfurt: Organisation der Abschleppvorg&auml;nge</h1>
+<p>Normalerweise schlie&szlig;t die Stadt Frankfurt Rahmenvertr&auml;ge mit Abschleppunternehmen, die dann von den st&auml;dtischen Beh&ouml;rden bei Bedarf angefordert werden k&ouml;nnen. Durch eine "Verwaltungspanne" <a href="#quellen_2"><sup>2</sup></a> hat die Stadt Frankfurt aktuell keinen g&uuml;ltigen Vertrag mit Abschleppunternehmen und "darf" daher nur eingeschr&auml;nkt abschleppen lassen um Klagen im Hinblick auf das europ&auml;ischen Vergaberechts aus dem Weg zu gehen. <a href="#quellen_2"><sup>2</sup></a> Ob diese Vertr&auml;ge auch f&uuml;r die Schutzpolizei gelten ist unklar.</p>
 
 <h1 id="quellennachweise">Quellennachweise</h1>
-<sup id="quellen_1"><sup>1</sup> <a href="https://twitter.com/Polizei_Ffm/status/936205764066242560">Offizielle Antwort der Polizei Frankfurt auf Twitter</a>, zuletzt gepr&uuml;ft am 07.02.2018 18 Uhr
+<sup id="quellen_1">1</sup> <a href="https://twitter.com/Polizei_Ffm/status/936205764066242560">Offizielle Antwort der Polizei Frankfurt auf Twitter</a>, zuletzt gepr&uuml;ft am 07.02.2018 18 Uhr
+<br>
+<sup id="quellen_2">2</sup> <a href="http://www.hessenschau.de/wirtschaft/frankfurt-kann-nicht-mehr-alle-falschparker-abschleppen-lassen,abschleppdienste-frankfurt-100.html">Bericht von hessenschau.de vom  17.01.18</a>, zuletzt gepr&uuml;ft am 08.02.2018 10 Uhr
 
 <?php include ("footer.html"); ?>
