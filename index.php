47fafdc9c45c5d4c43cee91647af8731f826759d
falschparkenffm <unknwon>
2018-02-08 10:08:57 +0100

    Fix layout and add "Elterntaxi"
    Closes #1

--- index.php
+++ index.php
@@ -5,18 +5,17 @@
 <h1>Das Ph&auml;nomen Falschparker</h1>
 <p>Nicht nur in Frankfurt behindern und gef&auml;hrden Falschparker andere Verkehrsteilnehmer wie Radfahrer, Fu&szlig;g&auml;nger, den &Ouml;PNV und nicht zuletzt auch andere Autofahrer. Das Problem kennt jede auf den Autoverkehr ausgerichtete Gro&szlig;stadt weltweit.</p>
 <p>In einigen St&auml;dten wird bei Falschparkern hart durchgegriffen - Frankfurt z&auml;hlt leider nicht dazu, da in der Regel lediglich die besonders g&uuml;nstigen "Kn&ouml;llchen" verteilt werden. Letztendlich kann aber <b>jeder</b> B&uuml;rger mit einfachen Mitteln Druck auf die eigentlich f&uuml;r die Durchsetzung der StVO zust&auml;ndigen Beh&ouml;rden aufbauen und diese zum Handeln bewegen.</p>
+<p>Gerade Kinder sind oft im Innenstadtbereich von zugeparkten Kreuzungen betroffen - w&auml;hrend Erwachsene meist &uuml;ber die stehenden KFZ schauen k&ouml;nnen ist dies f&uuml;r Kinder unm&ouml;glich. Hier sind besonders die im Kreuzungsbereich geparkten Fahrzeuge gef&auml;hrlich. Dazu kommen viele Situationen vor Schulen, Kinderg&auml;rten usw., bei denen Eltern die Kinder per Auto bis direkt vor die T&uuml;r bringen und dort nicht selten andere (Kinder) gef&auml;hrden.</p>
 <p>Diese Seite wird laufend aktualisiert und weiter entwickelt um allen genervten B&uuml;rgern einen einfachen Leitpfaden f&uuml;r den Kampf gegen Falschparker zu bieten.</p>
 
 <h1>M&ouml;glichkeiten f&uuml;r jedermann</h1>
-<p>Selbstverst&auml;ndlich kann nicht jeder alle der folgenden Punkte bearbeiten, allerdings ist jede Hilfe willkommen! Nur gemeinsam l&auml;sst sich das Problem in den Griff bekommen. Sofern es auf dieser Homepage bereits weiterf&uuml;hrende Informationen zu einem Themenbereich gibt ist die entsprechende Seite verlinkt. Alle weiteren folgen.</p> 
 <ol>
 <li><a href="verkehrsowianzeigen.html">Anzeigen einer Verkehrsordnungswidrigkeit</a></li>
 <li><a href="abschleppen.html">Abschleppen lassen / Informierung der Polizei</a></li>
 <li>&Ouml;ffentlichkeitswirksam Druck aus&uuml;ben</li>
-<li>Zust&auml;ndige Politiker ansprechen</li>
+<li>Zust&auml;ndige Politiker und Beh&ouml;rdenleiter ansprechen</li>
 <li>Sich Vereinen/Organisationen anschlie&szlig;en und dar&uuml;ber Druck aus&uuml;ben</li>
 </ol>
-
-<p>Gerade Eltern sind oft im Innenstadtbereich von zugeparkten Kreuzungen betroffen - w&auml;hrend Erwachsene meist &uuml;ber die stehenden KFZ schauen k&ouml;nnen ist dies f&uuml;r Kinder unm&ouml;glich. Hier sind besonders die im Kreuzungsbereich geparkten Fahrzeuge gef&auml;hrlich.</p>
+<p>Selbstverst&auml;ndlich kann nicht jeder alle der folgenden Punkte bearbeiten, allerdings ist jede Hilfe willkommen! Nur gemeinsam l&auml;sst sich das Problem in den Griff bekommen. Sofern es auf dieser Homepage bereits weiterf&uuml;hrende Informationen zu einem Themenbereich gibt ist die entsprechende Seite verlinkt. Alle weiteren folgen.</p> 
 
 <?php include ("footer.html"); ?>
