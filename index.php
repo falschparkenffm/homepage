28c3c0e6759636a34bd19817e6e265771faeb954
falschparkenffm <unknwon>
2018-02-08 13:02:31 +0100

    Fix typo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <p>Nicht nur in Frankfurt behindern und gef&auml;hrden Falschparker andere Verkehrsteilnehmer wie Radfahrer, Fu&szlig;g&auml;nger, den &Ouml;PNV und nicht zuletzt auch andere Autofahrer. Das Problem kennt jede auf den Autoverkehr ausgerichtete Gro&szlig;stadt weltweit.</p>
 <p>In einigen St&auml;dten wird bei Falschparkern hart durchgegriffen - Frankfurt z&auml;hlt leider nicht dazu, da in der Regel lediglich die besonders g&uuml;nstigen "Kn&ouml;llchen" verteilt werden. Letztendlich kann aber <b>jeder</b> B&uuml;rger mit einfachen Mitteln Druck auf die eigentlich f&uuml;r die Durchsetzung der StVO zust&auml;ndigen Beh&ouml;rden aufbauen und diese zum Handeln bewegen.</p>
 <p>Gerade Kinder sind oft im Innenstadtbereich von zugeparkten Kreuzungen betroffen - w&auml;hrend Erwachsene meist &uuml;ber die stehenden KFZ schauen k&ouml;nnen ist dies f&uuml;r Kinder unm&ouml;glich. Hier sind besonders die im Kreuzungsbereich geparkten Fahrzeuge gef&auml;hrlich. Dazu kommen viele Situationen vor Schulen, Kinderg&auml;rten usw., bei denen Eltern die Kinder per Auto bis direkt vor die T&uuml;r bringen und dort nicht selten andere (Kinder) gef&auml;hrden.</p>
-<p>Diese Seite ist ein Gemeinschaftsprojekt, wird laufend aktualisiert und weiter entwickelt um allen genervten B&uuml;rgern einen einfachen Leitpfaden f&uuml;r den Kampf gegen Falschparker zu bieten.</p>
+<p>Diese Seite ist ein Gemeinschaftsprojekt, wird laufend aktualisiert und weiter entwickelt um allen genervten B&uuml;rgern einen einfachen Leitfaden f&uuml;r den Kampf gegen Falschparker zu bieten.</p>
 
 <h1>M&ouml;glichkeiten f&uuml;r jedermann</h1>
 <ol>
@@ -19,6 +19,6 @@
 <p>Selbstverst&auml;ndlich kann nicht jeder alle der folgenden Punkte bearbeiten, allerdings ist jede Hilfe willkommen! Nur gemeinsam l&auml;sst sich das Problem in den Griff bekommen. Sofern es auf dieser Homepage bereits weiterf&uuml;hrende Informationen zu einem Themenbereich gibt ist die entsprechende Seite verlinkt. Alle weiteren folgen.</p> 
 
 <h1>Danksagungen</h1>
-<p>Ein Dank gilt dem Berliner <a href="https://twitter.com/Poliauwei">Poliauwei</a>, der dort hervorragende Arbeit leistet und viele, auch f&uuml;r Frankfurt relevante, Informationen erarbeitet hat. Diese sind bereits zum Teil in diese Seite eingeflossen, weitere werden folgen. F&uuml;r Berlin gibt es bereits einen deutlich detaillierteren <a href="https://www.autofreies-kreuzberg.de/radwege-frei.html">Leitpfaden zum Thema abschleppen</a>, der eine gewisse Vorbildfunktion hat.</p>
+<p>Ein Dank gilt dem Berliner <a href="https://twitter.com/Poliauwei">Poliauwei</a>, der dort hervorragende Arbeit leistet und viele, auch f&uuml;r Frankfurt relevante, Informationen erarbeitet hat. Diese sind bereits zum Teil in diese Seite eingeflossen, weitere werden folgen. F&uuml;r Berlin gibt es bereits einen deutlich detaillierteren <a href="https://www.autofreies-kreuzberg.de/radwege-frei.html">Leitfaden zum Thema abschleppen</a>, der eine gewisse Vorbildfunktion hat.</p>
 
 <?php include ("footer.html"); ?>
