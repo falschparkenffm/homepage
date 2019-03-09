6ee31efa77725e6c892a9025679773e0803affab
falschparkenffm <unknown>
2019-03-09 07:41:37 +0100

    Straffe Text und füge Hinweis zur Akteneinsicht hinzu

--- verkehrsowianzeigen.php
+++ verkehrsowianzeigen.php
@@ -2,8 +2,9 @@
 <title>Anzeigen einer Verkehrsordnungswidrigkeit</title>
 </head>
 <?php include ("menu.html"); ?>
-<p>Die "sanfte" Gegenma&szlig;nahme bei Falschparkern auf Rad- und Fu&szlig;g&auml;ngerwegen lautet "Vekehrsordnungswidrigkeitsanzeige". Sie kann von jeder Person gestellt werden, egal ob man selbst behindert wurde oder nicht. Inzwischen k&ouml;nnen diese Anzeigen auch per formloser E-Mail gestellt werden. Die Verwendung des offiziellen PDF-Formulars<a href="#quellen_3"><sup>3</sup></a> ist nicht mehr notwendig. <a href="#quellen_1"><sup>1</sup></a></p>
-<p>Aufgrund der geringen Bu&szlig;gelder bringen diese Anzeigen, wenn &uuml;berhaupt, nur etwas wenn viele Leute mitmachen! Falls m&ouml;glich ist das <a href="abschleppen.html">Abschleppen</a> unbedingt vorzuziehen.</p>
+<p>Jede Person kann eine "Vekehrsordnungswidrigkeitsanzeige" stellen, ob man selbst behindert wurde oder nicht ist dabei egal. Inzwischen k&ouml;nnen diese Anzeigen auch per formloser E-Mail gestellt werden. Die Verwendung des offiziellen PDF-Formulars<a href="#quellen_3"><sup>3</sup></a> ist nicht mehr notwendig. <a href="#quellen_1"><sup>1</sup></a></p>
+<p>Aufgrund der geringen Bu&szlig;gelder bringen diese Anzeigen, wenn &uuml;berhaupt, nur etwas wenn viele Leute mitmachen! Falls m&ouml;glich ist das <a href="abschleppen.html">Abschleppen</a> unbedingt vorzuziehen, da nur hiermit die Behinderung/Gef&auml;hrdung beseitig wird.</p>
+<p>Es ist f&uuml;r den Angezeigten m&ouml;glich die Anschrift des Anzeigenden &uuml;ber die Akteneinsicht zu erfahren.</p>
 
 <h1>Notwendige Schritte</h1>
 <ol>
