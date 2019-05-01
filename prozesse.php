f0d713195c570b5abcbae0e74b540879e07342e0
falschparkenffm <unknown>
2019-05-01 11:18:25 +0200

    Korrigiere Zeilenumbruch

--- prozesse.php
+++ prozesse.php
@@ -28,6 +28,7 @@
 <sup id="quellen_7">7</sup> <a href="https://www.ffh.de/nachrichten/hessen/rhein-main/toController/Topic/toAction/show/toId/172675/toTopic/weitere-schonzeit-fuer-falschparker-in-frankfurt.html" target="_blank" rel="noopener">Bericht von ffh.de vom 05.11.18</a>, nicht mehr abrufbar, zuletzt gepr&uuml;ft am 06.03.2019 14 Uhr
 <br>
 <sup id="quellen_8">8</sup> <a href="https://www.fr.de/frankfurt/falschparker-werden-haeufig-nicht-belangt-11826245.html" target="_blank" rel="noopener">Bericht von fr.de vom 06.03.19</a>, &uuml;ber <a href="https://web.archive.org/web/20190306132953/https://www.fr.de/frankfurt/falschparker-werden-haeufig-nicht-belangt-11826245.html">Web Archive</a>, zuletzt gepr&uuml;ft am 06.03.2019 14 Uhr
+<br>
 <sup id="quellen_9">9</sup> <a href="https://frankfurt.de/sixcms/detail.php?id=8440&_ffmpar[_id_inhalt]=35421125" target="_blank" rel="noopener">Pressemitteilung der Stadt Frankfurt vom 29.04.2019</a>, &uuml;ber <a href="https://web.archive.org/web/*/https://frankfurt.de/sixcms/detail.php?id=8440&_ffmpar[_id_inhalt]=35421125">Web Archive</a>, zuletzt gepr&uuml;ft am 30.04.2019 23 Uhr
 
 <?php include ("footer.html"); ?>
