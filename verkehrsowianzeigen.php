81a1bd791195d25df9c6551bc62b034393dde9fe
falschparkenffm <unknown>
2019-05-07 11:43:40 +0200

    Aktualisiere Text

--- verkehrsowianzeigen.php
+++ verkehrsowianzeigen.php
@@ -2,7 +2,7 @@
 <title>Anzeigen einer Verkehrsordnungswidrigkeit</title>
 </head>
 <?php include ("menu.html"); ?>
-<p>Jede Person kann eine "Vekehrsordnungswidrigkeitsanzeige" stellen, ob man selbst behindert wurde oder nicht ist dabei egal. Inzwischen k&ouml;nnen diese Anzeigen auch per formloser E-Mail gestellt werden. Die Verwendung des offiziellen PDF-Formulars<a href="#quellen_3"><sup>3</sup></a> ist nicht mehr notwendig. <a href="#quellen_1"><sup>1</sup></a></p>
+<p>Jede Person kann eine "Vekehrsordnungswidrigkeitsanzeige" stellen, ob man selbst behindert wurde oder nicht ist dabei egal. Die Anzeigen k&ouml;nnen auch per formloser E-Mail gestellt werden. Die Verwendung des offiziellen PDF-Formulars<a href="#quellen_3"><sup>3</sup></a> ist nicht mehr notwendig. <a href="#quellen_1"><sup>1</sup></a></p>
 <p>Aufgrund der geringen Bu&szlig;gelder bringen diese Anzeigen, wenn &uuml;berhaupt, nur etwas wenn viele Leute mitmachen! Falls m&ouml;glich ist das <a href="abschleppen.html">Abschleppen</a> unbedingt vorzuziehen, da nur hiermit die Behinderung/Gef&auml;hrdung beseitig wird.</p>
 <p>Es ist f&uuml;r den Angezeigten m&ouml;glich die Anschrift des Anzeigenden &uuml;ber die Akteneinsicht zu erfahren.</p>
 
@@ -10,17 +10,18 @@
 <ol>
 <li>Foto der Situation machen, auf dem sowohl das Fahrzeug inkl. Kennzeichen und der Weg/Beschilderung zu sehen ist</li>
 <li>Adresse inkl. Hausnummer merken/notieren (das spart sp&auml;ter Arbeit)</li>
+<li>Tatbestand herausfinden (halten auf Radweg, parken auf Gehweg; idealerweise unter Angabe der Tatbestandsnummer, die man <a href="https://www.kba.de/DE/ZentraleRegister/FAER/BT_KAT_OWI/bkat_owi_01_11_2017_pdf.pdf?__blob=publicationFile&v=2" target="_blank" rel="noopener">hier</a> finden kann)
 <li>Anzeige bei Polizeirevier der Wahl, <a href="mailto:owi.datenerfassung.amt32@stadt-frankfurt.de?Subject=Anzeige%20einer%20Verkehrsordnungswidrigkeit&amp;Body=Tattag%3A%20[DD.MM.YYYY]%0A%0ATatzeit%3A%20[HH:MM]%0A%0ATatort%3A%20[Strasse]%20[Hausnummer]%0A%0AKfz-Kennzeichen%3A%20[Kennzeichen]%0A%0AKfz-Farbe%20und%20-Marke%3A%20[Farbe]%20[Automarke]%0A%0AGenauer%20Tatvorwurf%3A%20Halten/Parken%20auf%20einem%20Radweg/Gehweg%20mit%20Behinderung%20von%20Radfahrern/Fußgängern,%20die%20ausweichen%20mussten.%0A%0AVollst&auml;ndiger%20Name%20und%20Anschrift%20des%20Anzeigenden%3A%0A[Vorname]%20[Nachname]%0A[Strasse]%20[Hausnummer]%0A[PLZ]%20[Wohnort]%0A%0AEin%20Beweisfoto%2C%20aus%20dem%20Kennzeichen%20und%20Tatvorwurf%20erkennbar%20hervorgehen%2C%20befindet%20sich%20im%20Anhang.">direkt per mailto-Link beim Ordnungsamt stellen</a> oder die <a href="#mail_vorlage">Mail-Vorlage</a> nutzen. <a href="#quellen_2"><sup>2</sup></a> <a href="#quellen_3"><sup>3</sup></a></li>
-<li>Fertig! Alles weitere &uuml;bernimmt die Stadt. Man bekommt generell keinerlei R&uuml;ckmeldung!</li>
+<li>Fertig! Alles weitere &uuml;bernimmt die Stadt. Man bekommt generell keinerlei R&uuml;ckmeldung, es sei denn es fehlen Daten!</li>
 </ol>
-<p>Da die <a href="https://www.wegeheld.org">Wegeheld-App</a> auch (falls gew&uuml;nscht) nur eine E-Mail generiert sollte auch diese Nutzung ohne Probleme funktionieren. Hierbei muss allerdings die korrekte Empf&auml;ngeradresse gepr&uuml;ft werden.</p>
+<p>Da die <a href="https://www.wegeheld.org">Wegeheld-App</a> auch (falls gew&uuml;nscht) nur eine E-Mail generiert sollte auch diese Nutzung ohne Probleme funktionieren.</p>
 
 <h1 id="mail_vorlage">E-Mail-Vorlage</h1>
 <p>
 Anstatt des oben verwendeten Direktlinks zum Mail-Programm kann auch folgender Text in eine Mail kopiert und an <i>owi.datenerfassung.amt32@stadt-frankfurt.de</i> gesendet werden:
 <br>
 <i>
-Tattag: 01.01.2018
+Tattag: 01.01.2019
 <br>
 <br>
 Tatzeit: 18:00 Uhr
