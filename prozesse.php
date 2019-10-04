44e54ea274d7acb4a2a6924e8d6c0e1a3807e269
falschparkenffm <unknown>
2019-10-04 20:25:33 +0200

    Füge Sprungmarken hinzu

--- prozesse.php
+++ prozesse.php
@@ -3,13 +3,13 @@
 </head>
 <?php include ("menu.html"); ?>
 <p>Diese Seite stellt nur einige Hintergrundinformationen bereit, diese sind f&uuml;r die allt&auml;gliche Arbeit normalerweise nicht erforderlich.</p>
-<h1>Ablauf der Verwaltungspanne bei Ausschreibung der Abschleppvertr&auml;ge</h1>
+<h1 id="ausschreibung_abschleppvertraege">Ablauf der Verwaltungspanne bei Ausschreibung der Abschleppvertr&auml;ge</h1>
 <p>Durch eine "Verwaltungspanne" <a href="#quellen_2"><sup>2</sup></a> hat die Stadt Frankfurt aktuell keinen g&uuml;ltigen Vertrag mit Abschleppunternehmen und "darf" daher nur eingeschr&auml;nkt abschleppen lassen um Klagen im Hinblick auf das europ&auml;ische Vergaberecht aus dem Weg zu gehen. <a href="#quellen_2"><sup>2</sup> <a href="#quellen_3"><sup>3</sup></a> Am 07.03.2018<a href="#quellen_4"><sup>4</sup></a> wurde berichtet, dass ab April 2018 wieder vermehrt abgeschleppt werden solle. Am 11.04.2018 wurde berichtet<a href="#quellen_5"><sup>5</sup></a>, dass nun etwas mehr Abschleppunternehmen beauftragt werden k&ouml;nnen. Die "richtigen" Abschleppvertr&auml;ge sind nach wie vor nicht aktiv. Am 19.10.2018 teilte die Stadtverwaltung mit <a href="#quellen_6"><sup>6</sup></a>, dass man ab dem 05.11.2018 wieder ganz normal abschleppen k&ouml;nne weil die Abschleppvertr&auml;ge ab dann wieder greifen. Bereits am 05.11.2018 gab es einen entsprechenden Bericht <a href="#quellen_7"><sup>7</sup></a>, wonach die angek&uuml;ndigten Vertr&auml;ge doch nicht gelten sollen und es noch einige Wochen dauern kann. Am 06.03.2019 wurde auf Nachfrage bekanntgegeben, dass es nach wie vor lediglich &Uuml;bergangsvertr&auml;ge gibt und im Jahr 2018 im Vergleich zu den Vorjahren &uuml;ber 50% weniger Abschleppvorg&auml;nge durchgef&uuml;hrt wurden.<a href="#quellen_8"><sup>8</sup></a> Am 29.04.2019 teilte die Verwaltung in einer Pressemitteilung mit, dass man zuk&uuml;nftig auf die europaweite Ausschreibung f&uuml;r Abschleppvertr&auml;ge verzichten und das Konzept einer eigenen Vermittlungsstelle beibehalten wird.<a href="#quellen_9"><sup>9</sup></p>
 
-<h1>Schutzpolizei: Organisation der Abschleppvorg&auml;nge</h1>
+<h1 id="schutz_orga_abschleppen">Schutzpolizei: Organisation der Abschleppvorg&auml;nge</h1>
 <p>Die Polizei Frankfurt organisiert ihre Abschleppauftr&auml;ge &uuml;ber die <a href="http://www.abschleppdienste-frankfurt.de">Firma Noss</a> <a href="#quellen_1"><sup>1</sup></a>.</p>
 
-<h1>Stadt Frankfurt: Organisation der Abschleppvorg&auml;nge</h1>
+<h1 id="stadt_orga_abschleppen">Stadt Frankfurt: Organisation der Abschleppvorg&auml;nge</h1>
 <p>Normalerweise schlie&szlig;t die Stadt Frankfurt Rahmenvertr&auml;ge mit Abschleppunternehmen, die dann von den st&auml;dtischen Beh&ouml;rden bei Bedarf angefordert werden k&ouml;nnen.</p>
 
 <h1 id="quellennachweise">Quellennachweise</h1>
