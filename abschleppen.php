b194304fc2e07402c71be810788cd5f0987409db
falschparkenffm <unknown>
2020-01-22 16:53:14 +0100

    Kürze Texte

--- abschleppen.php
+++ abschleppen.php
@@ -3,24 +3,13 @@
 </head>
 <?php include ("menu.html"); ?>
 <h1>Wieso abschleppen?</h1>
-<p>Die wirksamste Methode gegen Falschparker lautet "abschleppen", da dies die <b>einzige</b> M&ouml;glichkeit ist um eine Verkehrsbehinderung/-gef&auml;hrdung zu entfernen wenn der Fahrer nicht vor Ort zu ermitteln ist.</p>
-<p>Au&szlig;erdem gibt es durch die relativ hohen Kosten und dem entstandenen Aufwand oft einen Lerneffekt. Dies wissen auch die Kollegen der verschiedenen Beh&ouml;rden - die Folgen der reichlich verteilten und oft von Fahrern bereits einkalkulierten "Kn&ouml;llchen" sp&uuml;rt man in der gesamten Stadt. Hier ist es oftmals g&uuml;nstiger falsch zu parken anstatt ein legales Parkhaus aufzusuchen.</p>
+<p>Die wirksamste Methode gegen Falschparker ist "abschleppen", weil <b>nur</b> so eine Verkehrsbehinderung/-gef&auml;hrdung <b>beseitigt</b> wird.</p>
 
-<h1>Wie geht das?</h1>
-<p>In der Theorie ist es ganz einfach: Man ruft eine der <a href="behoerden.html">Beh&ouml;rden</a> an, diese r&uuml;cken aus und veranlassen den Abschleppvorgang. Verkehrsbehinderung entfernt, Fall erledigt.</p>
-<p>In der Praxis sieht das leider etwas anders aus. Bis zum Punkt "ausr&uuml;cken" funktioniert es wie oben beschrieben, lediglich der letzte und vor allem entscheidende Schritt wird in aller Regel nicht ausgef&uuml;hrt. Wieso? Die Liste der Ausreden w&uuml;rde den Umfang dieser Seite sprengen.</p>
-<p>Wer vor Ort mit den Polizisten diskutieren m&ouml;chte findet <a href="media/polizei_handout.pdf">hier</a> eine Sammlung von Argumentationshilfen. Das Handout darf gerne an Polzisten und Mitstreiter verteilt werden.</p>
+<h1>Was kann ich tun? Wie geht das?</h1>
+<p>Ruf die Polizei (siehe <a href="behoerden.html">Beh&ouml;rden</a>) an und melde eine "Verkehrsbehinderung" (wichtiges Wort), ggf. mit "Gef&auml;hrdung". Die Person am anderen Ende wird dann fragen welche Art von Verkehrsbehinderung genau vorliegt und um welchen Ort es geht. Jetzt kann man sagen, dass es sich z.B. um einen durch KFZ zugeparkten Radweg, Gehweg o.&auml;. handelt. Manchmal wird man auch gebeten das Kennzeichen zu nennen, sofern man noch in der Nähe ist. Es wird eine Polizeistreife geschickt, dabei man muss <b>nicht</b> vor Ort warten. Der Falschparker sollte dann durch die Polizei abgeschleppt werden. Dabei hast du keinerlei Risiko! Nat&uuml;rlich kann und darf man sich den Einsatz auch angucken und auch (freundlich!) mit den Polizist*innen ins Gespr&auml;ch kommen.</p>
 
-<h1>Was sage ich am Telefon?</h1>
-<p>Man teil mit, dass man gerne eine "Verkehrsbehinderung" (wichtiges Wort), ggf. mit "Gef&auml;hrdung", melden m&ouml;chte. Die Person am anderen Ende wird dann fragen welche Art von Verkehrsbehinderung genau vorliegt und um welchen Ort es geht - nun kann man sagen dass es sich z.B. um einen durch KFZ zugeparkten Radweg, Gehweg o.&auml;. handelt. Manchmal wird man auch noch nach dem Kennzeichen gefragt.</p>
-<p>Das ist einfach, oder? Wer m&ouml;chte kann auch fragen wie lange es circa dauern wird bis jemand vor Ort sein wird. Falls diese Zeit (z.B. bei einer konkreten Gef&auml;hrdung) zu weit in der Zukunft liegt kann man auch zus&auml;tzlich die <a href="behoerden.html#schutzpolizei">Schutzpolizei</a> informieren. Hierbei dann bitte darauf hinweisen, dass man die st&auml;dtische Verkerhspolizei bereits informiert hat - ansonsten geben die Kollegen dort es intern auch nur an die selbe Stelle weiter.</p>
-
-<h1>Wieso soll ich anrufen, wenn sich die Beh&ouml;rden weigern abzuschleppen?</h1>
-<p>Die Antwort ist simpel: Zu Beginn brauchen die Beh&ouml;rden Druck aus der Bev&ouml;lkerung um sich dem zus&auml;tzlichen Aufwand auszusetzen. Dieser kann sehr einfach z.B. durch telefonische Meldungen von Vekehrsbehinderungen aufgebaut werden.</p>
-<p>Anfangs ist der Aufwand f&uuml;r die Mitarbeiter der Beh&ouml;rden sehr hoch, da sie bei jedem Falschparker vor Ort warten m&uuml;ssen bis der Abschleppvorgang abgeschlossen ist. Das kostet viel Zeit, zeigt aber <b>nachhaltig</b> Wirkung. Auf Dauer sinkt also die Arbeitslast.</p>
-<p>Und auch wenn nicht abgeschleppt wird: die Polizei zeigt an den (oft selben Stellen) Pr&auml;senz und sei es nur durch das Verteilen von "Kn&ouml;llchen". Auch das ist besser als nichts. Au&szlig;erdem taucht <b>jeder</b> Einsatz in einer Statistik auf - wenn sich dort das Thema "Falschparker/Verkehrsbehinderung" h&auml;uft wird es fr&uuml;her oder sp&auml;ter Aufmerksamkeit erregen.</p>
-
-<h1>Muss ich vor Ort warten?</h1>
-<p>Nein! Man ist nicht verpflichtet zu warten. Man kann auch die Meldung an die entsprechende Beh&ouml;rde geben und seinen Weg fortsetzen. Wer m&ouml;chte kann nat&uuml;rlich gerne vor Ort bleiben und schauen was passiert - oder sogar mit den Polizisten sprechen, wenn diese anders handeln als erwartet.</p>
+<h1>Was tun bei Problemen?</h1>
+<p>Wichtig: ruhig und sachlich bleiben! Falls sich die Polizei z.B. weigert Falschparker abzuschleppen, mache bitte folgendes: schreibe dir Datum, Uhrzeit, Ort und im Idealfall die Namen oder das Autokennzeichen der Polizist*innen auf und mache ein paar aussagekr&auml;ftige Fotos. Schreibe au&szlig;erdem genau auf wieso angeblich nicht abgeschleppt werden k&ouml;nnte. Lass uns die Daten am besten &uuml;ber <a href="https://www.twitter.com/FalschparkenFFM">Twitter</a> zukommen, wir k&uuml;mmern uns dann um die Nachbereitung des Einsatzes.</p>
+<p>Alternativ kannst du auch vor Ort in die Diskussion mit der Polizei gehen, daf&uuml;r haben wir extra eine <a href="media/polizei_handout.pdf">hier</a> eine Sammlung an Argumentationshilfen. Das Dokument kann gerne an die Polizei oder Mitstreiter*innen verteilt werden! Melde dich anschlie&szlig;end bitte unbedingt auch bei uns und teile deine Erlebnisse.</p>
 
 <?php include ("footer.html"); ?>
