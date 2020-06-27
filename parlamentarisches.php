163829bb37c07dd19e7dc2bebbf28759a4d1836e
falschparkenffm <unknown>
2020-06-27 22:23:31 +0200

    Füge parlamentarische Anfragen hinzu

---
+++ parlamentarisches.php
@@ -0,0 +1,77 @@
+<?php include ("header.html"); ?>
+<title>Parlamentarische Anfragen</title>
+</head>
+<?php include ("menu.html"); ?>
+<p>Hier sammeln wir parlamentarische Anfragen und zugeh&ouml;rige Presseberichte rund um das Thema Falschparken in Frankfurt. Die Seite dient vor allem als Nachschlagewerk und Inspiration f&uuml;r andere Aktive, die in ihren St&auml;dten/Bundesl&auml;ndern Anfragen stellen m&ouml;chten.</p>
+
+<p>Wir unterst&uuml;tzen und beraten partei&uuml;bergreifend rund um das Thema Falschparker und helfen dabei pr&auml;zise Anfragen zu formulieren. Bei Interesse bitte einfach Kontakt aufnehmen.</p>
+
+<h1>Anfragen im hessischen Landtag</h1>
+<p><ol>
+<li>Kleine Anfrage "Falschparker und unklare Zuständigkeiten zwischen Hilfspolizei und Landespolizei" der DIE LINKE-Fraktion vom 20.08.2019<a href="#quellen_1"><sup>1</sup></a>
+<br>
+<strong>Themen:</strong> Grunds&auml;tzliches Gefahrenpotential von Falschparkern, Zusta&uml;ndigkeiten zwischen Beh&ouml;rden, "Regelf&auml;lle" f&uuml;r Umgang mit Falschparkern, Nutzung des Notrufs</li>
+
+<li>Kleine Anfrage "Falschparker und Abschleppvorgänge durch die Landespolizei" der DIE LINKE-Fraktion vom 20.08.2019<a href="#quellen_2"><sup>2</sup></a>
+<br>
+
+<strong>Themen:</strong> Ermessensspielraum bei Abschleppma&szlig;nahmen, Pflicht zur Suche nach Ansprechpartner vor Abschleppma&szlig;nahme, Haftung der Polizist*innen, Abschleppzahlen f&uuml;r hessische Polizeipr&auml;sidien</li>
+<li>Presseartikel in der Frankfurter Rundschau vom 25.08.2019 zu den beiden kleinen Anfragen 1 und 2<a href="#quellen_3"><sup>3</sup></a></li>
+</ol></p>
+
+<h1>Anfragen im Frankfurter Stadtparlament bzw. den Ortsbeir&auml;ten</h1>
+<p><ol>
+<li>Bericht des Magistrats vom 18.11.2019 auf eine Anfrage der DIE LINKE.-Fraktion ("Wie viele wurden abgeschleppt?")<a href="#quellen_4"><sup>4</sup></a>
+<br>
+<strong>Themen:</strong> Zahlen der privaten Ordnungswidrigkeitsanzeigen 2015-2018 inkl. Bearbeitungszeiten, verj&auml;hrte Privatanzeigen, Anzahl sta&uml;dtische Abschleppvorg&auml;nge 2015-2018</li>
+
+<li>Stellungnahme des Magistrats vom 15.05.2020 zur Anfrage des Ortsbeirats 5 ("Verkehrsordnungswidrigkeiten besser verfolgen")<a href="#quellen_5"><sup>5</sup></a></li>
+<br>
+<strong>Themen:</strong> Verfolgung von privaten Verkehrsordnungswidrigkeitsanzeigen
+
+<li>Bericht des Magistrats vom 25.03.2019 zur Anfrage der FRANKFURTER-Fraktion ("Freie Gehwege in allen Stadtteilen")<a href="#quellen_6"><sup>6</sup></a></li>
+<br>
+<strong>Themen:</strong> Freie Gehwege f&uuml;r Fu&szlig;verkehr, Fremdnutzung von Garagen, mangelnde Polizeiarbeit</li>
+
+<li>Bericht des Magistrats zur Anfrage des Ortsbeirats 1 ("Parkplätze frei räumen von Schrottfahrzeugen")<a href="#quellen_7"><sup>7</sup></a></li>
+<br>
+<strong>Themen:</strong> Dauer der Abschleppma&szlig;nahmen von "Schrottfahrzeugen", Prozesse zur Entfernung von "Schrottfahrzeugen"</li>
+
+<li>Bericht des Magistrats zur Anfrage des Ortsbeirats 4 ("Exzessives Parken auf Gr&uuml;nfl&auml;chen w&auml;hrend der Dippemess")<a href="#quellen_8"><sup>8</sup></a></li>
+<br>
+<strong>Themen:</strong> Massenhafte Parkverst&ouml;&szlig;e w&auml;hrend der Dippemess in den Gr&uuml;nfl&auml;chen des Ostparks
+
+<li>Bericht des Magistrats zur Anfrage der DIE LINKE.-Fraktion ("Abschleppdienstleistungen")<a href="#quellen_9"><sup>9</sup></a></li>
+<br>
+<strong>Themen:</strong> Details zur verz&ouml;gerten Vergabe der st&auml;dtischen Abschleppdienstleistungen an private Unternehmen
+</ol></p>
+
+<h1 id="quellennachweise">Quellennachweise</h1>
+<sup id="quellen_1">1</sup> <a href="http://starweb.hessen.de/cache/DRS/20/2/00932.pdf" target="_blank" rel="noopener">Kleine Anfrage im hessischen Landtag zu den Zust&auml;ndigkeiten der verschiedenen Beh&ouml;rden</a>, &uuml;ber <a href="https://web.archive.org/web/20191004184617/http://starweb.hessen.de/cache/DRS/20/2/00932.pdf">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 20 Uhr
+<br>
+
+<sup id="quellen_2">2</sup> <a href="http://starweb.hessen.de/cache/DRS/20/3/00933.pdf" target="_blank" rel="noopener">Kleine Anfrage im hessischen Landtag zu den Zust&auml;ndigkeiten der verschiedenen Beh&ouml;rden und Prozessen</a>, &uuml;ber <a href="https://web.archive.org/web/20191004184644/starweb.hessen.de/cache/DRS/20/3/00933.pdf">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 20 Uhr
+<br>
+
+<sup id="quellen_3">3</sup> <a href="https://www.fr.de/rhein-main/zugeparkte-radwege-polizei-soll-abschleppen-lassen-12943374.html" target="_blank" rel="noopener">Pressebericht in der Frankfurter Rundschau vom 25.08.2019 zu den Anfragen 1 und 2 im hessischen Landtag</a>, &uuml;ber <a href="https://web.archive.org/web/20190825213337/www.fr.de/rhein-main/zugeparkte-radwege-polizei-soll-abschleppen-lassen-12943374.html">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 20 Uhr
+<br>
+
+<sup id="quellen_4">4</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_414_2019%27" target="_blank" rel="noopener">Bericht B 414/2019 des Magistrats zur Anfrage der DIE LINKE.-Fraktion</a>, &uuml;ber <a href="https://web.archive.org/web/20200627195803/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_414_2019%27">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 20 Uhr
+<br>
+
+<sup id="quellen_5">5</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27ST_946_2020%27" target="_blank" rel="noopener">Stellungnahme ST 946/2020 des Magistrats</a>, &uuml;ber <a href="https://web.archive.org/web/20200627194414/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27ST_946_2020%27">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 21 Uhr
+<br>
+
+<sup id="quellen_6">6</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_99_2019%27" target="_blank" rel="noopener">Bericht B 99/2019 des Magistrats zur Anfrage der FRANKFURTER-Fraktion</a>, &uuml;ber <a href="https://web.archive.org/web/20190327214914/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_99_2019%27">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 21 Uhr
+<br>
+
+<sup id="quellen_7">7</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27ST_977_2020%27" target="_blank" rel="noopener">Stellungnahme ST 977/2020 des Magistrats zur Anfrage des Ortsbeirats 1</a>, &uuml;ber <a href="https://web.archive.org/web/20200627194952/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27ST_977_2020%27">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 21 Uhr
+<br>
+
+<sup id="quellen_8">8</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27ST_1535_2019%27" target="_blank" rel="noopener">Stellungnahme ST 1535/2019 des Magistrats zur Anfrage des Ortsbeirats 4</a>, &uuml;ber <a href="https://web.archive.org/web/20200627200527/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27ST_1535_2019%27">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 21 Uhr
+<br>
+
+<sup id="quellen_9">9</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_314_2019%27" target="_blank" rel="noopener">Bericht B 314/2019 des Magistrats zur Anfrage der DIE LINKE.-Fraktion</a>, &uuml;ber <a href="https://web.archive.org/web/20200627201307/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_314_2019%27">Web Archive</a>, zuletzt gepr&uuml;ft am 27.06.2020 21 Uhr
+<br>
+
+<?php include ("footer.html"); ?>
