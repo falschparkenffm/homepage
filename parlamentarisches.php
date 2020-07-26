a557727419f842bf36fa3c2fd2687081b2b3c582
falschparkenffm <unknown>
2020-07-26 18:56:39 +0200

    Ergänze weitere Anfrage zu Gehwegbreiten und mehr Leerzeilen für die Lesbarkeit

--- parlamentarisches.php
+++ parlamentarisches.php
@@ -11,11 +11,12 @@
 <li>Kleine Anfrage "Falschparker und unklare Zuständigkeiten zwischen Hilfspolizei und Landespolizei" der DIE LINKE-Fraktion vom 20.08.2019<a href="#quellen_1"><sup>1</sup></a>
 <br>
 <strong>Themen:</strong> Grunds&auml;tzliches Gefahrenpotential von Falschparkern, Zusta&uml;ndigkeiten zwischen Beh&ouml;rden, "Regelf&auml;lle" f&uuml;r Umgang mit Falschparkern, Nutzung des Notrufs</li>
+<br>
 
 <li>Kleine Anfrage "Falschparker und Abschleppvorgänge durch die Landespolizei" der DIE LINKE-Fraktion vom 20.08.2019<a href="#quellen_2"><sup>2</sup></a>
 <br>
-
 <strong>Themen:</strong> Ermessensspielraum bei Abschleppma&szlig;nahmen, Pflicht zur Suche nach Ansprechpartner vor Abschleppma&szlig;nahme, Haftung der Polizist*innen, Abschleppzahlen f&uuml;r hessische Polizeipr&auml;sidien</li>
+<br>
 <li>Presseartikel in der Frankfurter Rundschau vom 25.08.2019 zu den beiden kleinen Anfragen 1 und 2<a href="#quellen_3"><sup>3</sup></a></li>
 </ol></p>
 
@@ -24,26 +25,37 @@
 <li>Bericht des Magistrats vom 18.11.2019 auf eine Anfrage der DIE LINKE.-Fraktion ("Wie viele wurden abgeschleppt?")<a href="#quellen_4"><sup>4</sup></a>
 <br>
 <strong>Themen:</strong> Zahlen der privaten Ordnungswidrigkeitsanzeigen 2015-2018 inkl. Bearbeitungszeiten, verj&auml;hrte Privatanzeigen, Anzahl sta&uml;dtische Abschleppvorg&auml;nge 2015-2018</li>
+<br>
 
 <li>Stellungnahme des Magistrats vom 15.05.2020 zur Anfrage des Ortsbeirats 5 ("Verkehrsordnungswidrigkeiten besser verfolgen")<a href="#quellen_5"><sup>5</sup></a></li>
 <br>
 <strong>Themen:</strong> Verfolgung von privaten Verkehrsordnungswidrigkeitsanzeigen
+<br>
 
 <li>Bericht des Magistrats vom 25.03.2019 zur Anfrage der FRANKFURTER-Fraktion ("Freie Gehwege in allen Stadtteilen")<a href="#quellen_6"><sup>6</sup></a></li>
 <br>
 <strong>Themen:</strong> Freie Gehwege f&uuml;r Fu&szlig;verkehr, Fremdnutzung von Garagen, mangelnde Polizeiarbeit</li>
+<br>
 
 <li>Bericht des Magistrats zur Anfrage des Ortsbeirats 1 ("Parkplätze frei räumen von Schrottfahrzeugen")<a href="#quellen_7"><sup>7</sup></a></li>
 <br>
 <strong>Themen:</strong> Dauer der Abschleppma&szlig;nahmen von "Schrottfahrzeugen", Prozesse zur Entfernung von "Schrottfahrzeugen"</li>
+<br>
 
 <li>Bericht des Magistrats zur Anfrage des Ortsbeirats 4 ("Exzessives Parken auf Gr&uuml;nfl&auml;chen w&auml;hrend der Dippemess")<a href="#quellen_8"><sup>8</sup></a></li>
 <br>
 <strong>Themen:</strong> Massenhafte Parkverst&ouml;&szlig;e w&auml;hrend der Dippemess in den Gr&uuml;nfl&auml;chen des Ostparks
+<br>
 
 <li>Bericht des Magistrats zur Anfrage der DIE LINKE.-Fraktion ("Abschleppdienstleistungen")<a href="#quellen_9"><sup>9</sup></a></li>
 <br>
 <strong>Themen:</strong> Details zur verz&ouml;gerten Vergabe der st&auml;dtischen Abschleppdienstleistungen an private Unternehmen
+<br>
+
+<li>Bericht des Magistrats zur Anfrage der FRANKFURTER-Fraktion ("Erlaubtes Gehwegparken in Frankfurt")<a href="#quellen_11"><sup>11</sup></a></li>
+<br>
+<strong>Themen:</strong> Details zum angeordneten Gehwegparkens und zur notwendigen Restbreite bei Gehwegen in Frankfurt
+<br>
 </ol></p>
 
 <h1>Sonstige Anfragen</h1>
@@ -82,6 +94,9 @@
 <br>
 
 <sup id="quellen_10">10</sup> <a href="https://fragdenstaat.de/anfrage/abschleppen-und-umsetzen-von-kraftfahrzeugen-gehwegrestbreite" target="_blank" rel="noopener">Private Anfrage &uuml;ber fragdenstaat.de zu Richtlinien f&uuml;r Regelf&auml;lle des Abschleppens und Restgehwegbreite</a>, &uuml;ber <a href="https://fragdenstaat.de/anfrage/abschleppen-und-umsetzen-von-kraftfahrzeugen-gehwegrestbreite">Web Archive</a>, zuletzt gepr&uuml;ft am 12.07.2020 11 Uhr
+<br>
+
+<sup id="quellen_11">11</sup> <a href="https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME=%27B_228_2020%27" target="_blank" rel="noopener">Bericht B 220/2020 des Magistrats zur Anfrage der FRANKFURTER-Fraktion</a>, &uuml;ber <a href="https://web.archive.org/web/*/https://www.stvv.frankfurt.de/PARLISLINK/DDW?W=DOK_NAME%3D'B_228_2020'">Web Archive</a>, zuletzt gepr&uuml;ft am 26.07.2020 19 Uhr
 <br>
 
 <?php include ("footer.html"); ?>
